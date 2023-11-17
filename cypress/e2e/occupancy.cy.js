41c25d98a42c706b2ea4a549bfd505a06f82e5a2
suparnaneeraj <suparnaneeeraj94@gmail.com>
2023-11-17 13:55:12 +0100

    code formatted

--- cypress/e2e/occupancy.cy.js
+++ cypress/e2e/occupancy.cy.js
@@ -1,6 +1,4 @@
-
 describe('Verify the count of premium and economy rooms occupied along with the amount earned ', () => {
-    let getText
 
     before(function () {
         cy.fixture('data.json').as('data')
@@ -9,32 +7,27 @@ describe('Verify the count of premium and economy rooms occupied along with the 
     it('should correctly calculate the premium and economy rooms usage', function () {
         cy.visit(this.data.url);
         (this.data.test_data).forEach(element_data => {
-            cy.log(element_data)
             const data_object = JSON.parse(JSON.stringify(element_data))
             cy.get('div.main').contains("Premium Rooms:").parent('div').find('input').clear()
             cy.get('div.main').contains("Premium Rooms:").parent('div').find('input').type(data_object.free_premium_rooms)
+
             cy.get('div.main').contains("Economy Rooms:").parent('div').find('input').clear()
             cy.get('div.main').contains("Economy Rooms:").parent('div').find('input').type(data_object.free_economy_rooms)
             cy.get('button[type="submit"]').click()
 
-            var array_usage_values = []
+            let occupancyData = []
             let i = 0
-            cy.get('div.main > div').find('p').each(($el, index, $list) => {
-                const result_text = $el.text()
-
-                const array_text = result_text.split(":")
-
-                array_usage_values[i] = array_text[1].trim()
+            cy.get('div.main > div').find('p').each((element) => {
+                const nodeContent = element.text()
+                const nodeValue = nodeContent.split(":")
+                occupancyData[i] = nodeValue[1].trim()
                 i++;
-            }).then(($list) => {
-                expect(array_usage_values[0]).to.equal(data_object.free_premium_rooms)
-                expect(array_usage_values[1]).to.equal(data_object.free_economy_rooms)
-                expect(array_usage_values[2]).to.equal(data_object.usage_premium)
-                expect(array_usage_values[3]).to.equal(data_object.usage_economy)
-
+            }).then((roomData) => {
+                expect(occupancyData[0]).to.equal(data_object.free_premium_rooms)
+                expect(occupancyData[1]).to.equal(data_object.free_economy_rooms)
+                expect(occupancyData[2]).to.equal(data_object.usage_premium)
+                expect(occupancyData[3]).to.equal(data_object.usage_economy)
             })
-
         });
-
     })
 })
