ad5a77ef70e263ef9c0a13f89e697315c05afd99
suparnaneeraj <suparnaneeeraj94@gmail.com>
2023-11-17 14:54:22 +0100

    added report generation using mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
     e2e: {
+        reporter: "mochawesome",
+        reporterOptions: {
+            charts: true,
+            html: false,
+            json: true,
+            overwrite: false,
+            reportDir: "cypress/reports"
+        },
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
+
     },
 });
